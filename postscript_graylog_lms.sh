df814f367de123f69ceb165c091e4dc628490f21
danko1981 <145504669+danko1981@users.noreply.github.com>
2024-01-31 13:58:12 +0100

    Update postscript_graylog_lms.sh

--- postscript_graylog_lms.sh
+++ postscript_graylog_lms.sh
@@ -24,7 +24,7 @@ apt-get install apt-transport-https uuid-runtime pwgen net-tools -y
 wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
-dpkg -i ./libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb
+dpkg -i ./libssl1.1_1.1.1f-1ubuntu2_amd64.deb
 apt-get update
 apt-get install -y mongodb-org
 systemctl daemon-reload
